554e536938b3706a563c7f0167b8d2a116c0933a
phucvo27 <learninge27@gmail.com>
2020-03-12 15:32:46 +0700

    init app

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const { app } = require('./app');
+
+
+
+const PORT = process.env.PORT || 5000;
+
+
+const httpServer = http.createServer(app);
+
+
+httpServer.listen(PORT, ()=>{
+    console.log(`Server is starting at : ${PORT}`)
+})
