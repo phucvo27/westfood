e6b8a8d44bf7206290bff7f52eb270e4bea6f836
phucvo27 <learninge27@gmail.com>
2020-04-23 15:11:38 +0700

    updated route on nodejs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const STATIC_URL = `${__dirname}/client/build`;
 
 app.use(express.static(STATIC_URL));
 
-app.get('/', (req, res)=>{
+app.get('*', (req, res)=>{
     res.sendFile(`${STATIC_URL}/index.html`)
 })
 app.get('/service-worker.js', (req, res)=>{
